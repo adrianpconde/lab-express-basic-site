8d5b6e39a991fb7fefd1659a45db5b24f98f4470
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-06-18 14:03:14 +0200

    Page listening to port 3000, gitignore updated, views files & public bin created

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get("/", (req, res, next) => {
+    console.log('Ok. Hola')
+    res.sendFile(`${__dirname}/views/index.html`)
+});
+
+app.get("/about", (req, res, next) => {
+    res.sendFile(`${__dirname}/views/about.html`)
+});
+
+app.get("/works", (req, res, next) => {
+    res.sendFile(`${__dirname}/views/works.html`)
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Application listening at port ${port}`))

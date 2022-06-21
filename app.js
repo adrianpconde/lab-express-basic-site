16455cb7f6e3391092a1162d05f1bf321227911a
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-06-21 11:07:59 +0200

    Bootstrap dependencies & photo gallery page added

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
 app.get("/", (req, res, next) => {
-    console.log('Ok. Hola')
-    res.sendFile(`${__dirname}/views/index.html`)
+  console.log("Ok. Hola");
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get("/about", (req, res, next) => {
-    res.sendFile(`${__dirname}/views/about.html`)
+  res.sendFile(`${__dirname}/views/about.html`);
 });
 
 app.get("/works", (req, res, next) => {
-    res.sendFile(`${__dirname}/views/works.html`)
+  res.sendFile(`${__dirname}/views/works.html`);
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(`${__dirname}/views/gallery.html`);
 });
 
 const port = 3000;
-app.listen(port, () => console.log(`Application listening at port ${port}`))
+app.listen(port, () => console.log(`Application listening at port ${port}`));
